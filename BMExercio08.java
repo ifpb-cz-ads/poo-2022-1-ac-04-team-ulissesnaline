5c809ace5034712c4e3e0822bf22d8a40c3bdf7b
ulisses <iamurise@gmail.com>
2022-06-04 22:24:48 -0300

    add BMExercio08.java

---
+++ BMExercio08.java
@@ -0,0 +1,8 @@
+public class BMExercio08 {
+    public static void main(String[] args) {
+        System.out.println("Multiples of 3 between 1 and 100: ");
+        for (int i = 3; i <= 100; i += 3) {
+            System.out.print(i + "\t");
+        }
+    }
+}
